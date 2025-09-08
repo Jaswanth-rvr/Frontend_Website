10132c4acfde267de5bfcb1f51cd741678376158
Jaswanth-rvr <jaswanthbollina1@gmail.com>
2025-09-08 21:38:34 +0530

    Update constants.ts

--- constants.ts
+++ constants.ts
@@ -6,6 +6,6 @@ export const NAV_LINKS: NavLink[] = [
   { name: 'Work', href: '#projects' },
   { name: 'Services', href: '#services' },
   { name: 'Clients', href: '#clients' },
-  { name: 'About', href: '#' },
-  { name: 'Contact', href: '#' },
+  { name: 'About', href: '#about' },
+  { name: 'Contact', href: '#contact' },
 ];
