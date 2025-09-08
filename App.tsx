3ef5ac82b268221711b200df03faf1681c43e1dc
Jaswanth-rvr <jaswanthbollina1@gmail.com>
2025-09-08 21:34:24 +0530

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -5,6 +5,8 @@ import Hero from './components/Hero';
 import Clients from './components/Clients';
 import Services from './components/Services';
 import Projects from './components/Projects';
+import Contact from './components/Contact';
+import About from './components/About';
 
 const App: React.FC = () => {
   return (
@@ -12,9 +14,12 @@ const App: React.FC = () => {
       <Navbar />
       <main>
         <Hero />
+        <About/>
         <Clients />
         <Services />
         <Projects />
+        <Contact/>
+        
       </main>
       <footer className="bg-slate-900 py-8 text-center text-gray-400">
         <p>&copy; {new Date().getFullYear()} Creative Agency. All Rights Reserved.</p>
